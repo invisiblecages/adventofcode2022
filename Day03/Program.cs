bb195168e790a53e5c27a90b108debee356bd1db
Wenzel <70312879+invisiblecages@users.noreply.github.com>
2022-12-02 22:46:54 +0100

    Add project files.

---
+++ Day03/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+
+*/
+namespace Day03;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string workingDirectory = Environment.CurrentDirectory;
+        string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.Parent.FullName;
+        string path = Path.Combine(projectDirectory, "day03.txt");
+
+        string[] data = File.ReadAllLines(path);
+    }
+}
